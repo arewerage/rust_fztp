2205b9dc30eddfad545d64e4d95d4fd290bc4664
arewerage <arewerage@gmail.com>
2023-10-10 22:09:43 +0900

    feat(books/00): complete ch02-00

---
+++ 00_books/00_the_rust_programming_language/README.md
@@ -0,0 +1,6 @@
+# Materials:
+
+## Chapter 2 (Guessing Game):
+
+- Let's Get Rusty - https://youtu.be/H0xBSbnQYds?si=w2twCVWpXo8f_2Ji
+- The Dev Method - https://youtu.be/ub2jqWeez9c?si=Bkr7OgD4Kuls__3b
--- 00_books/00_the_rust_programming_language/ch02-00-guessing-game-tutorial/src/main.rs
+++ 00_books/00_the_rust_programming_language/ch02-00-guessing-game-tutorial/src/main.rs
@@ -1,3 +1,37 @@
+use colored::Colorize;
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("\nPlease input your guess: ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+        }
+    }
 }
